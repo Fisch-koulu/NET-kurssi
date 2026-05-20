ea50db1fdcb6a45f8daf863fc33ceadf41d0a33d
Savelius Milla <s2401247@edu.bc.fi>
2026-05-20 14:32:55 +0300

    oman komponenttikirjasto
    tein transformin :)

---
+++ Ruptus_kirjasto/Transform.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+
+namespace Ruptus_kirjasto
+{
+    internal class Transform
+    {
+        private Vector2 position;   //paikka
+        private Vector2 direction;  //suunta
+        private float speed;    //nopeus
+
+        //muu koodi voi katsoa ja muuttaa
+        public Vector2 Position { get { return position; } set { position = value; } } 
+        public Vector2 Direction { get { return direction; } set { direction = value; } }
+
+        public Transform(Vector2 position, Vector2 direction, float speed)
+        {
+            this.position = position;
+            this.direction = direction;
+            this.speed = speed;
+        }
+
+        /// <summary>
+        /// Liikuttaa objektia.
+        /// </summary>
+        public void Move()
+        {
+            position += direction * speed * Raylib.GetFrameTime();
+            //olisin laittanut ettei voi mennä näytöltä ohi tässä, mutta tämä ei ole vielä collderia
+            //esim: position.X = Math.Clamp(position.X, (0+size/2), (Raylib.GetScreebWidth()-size/2))
+        }
+
+        /// <summary>
+        /// Liikkuu näppäinten mukaan.
+        ///</summary>
+        public void MoveInput(KeyboardKey upKey, KeyboardKey downKey, KeyboardKey leftKey, KeyboardKey rightKey)
+        {
+            if (Raylib.IsKeyDown(upKey))
+            {
+                direction = new Vector2(0, -1);
+            }
+            else if (Raylib.IsKeyDown(downKey))
+            {
+                direction = new Vector2(0, 1);
+            }
+            //else if varmistaa, ettei jää jumiin, kun painaa vastakkaisia samaan aikaan.
+            if (Raylib.IsKeyDown(leftKey))
+            {
+                direction = new Vector2(-1, 0);
+            }
+            else if (Raylib.IsKeyDown(rightKey))
+            {
+                direction = new Vector2(1, 0);
+            }
+
+            //lopuksi liikutaan
+            Move();
+        }
+    }
+}
